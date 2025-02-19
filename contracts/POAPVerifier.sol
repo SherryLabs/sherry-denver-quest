44749a491fef4e00a272a261c9b68dd9ce4a2823
Sebyte <sebaponti@gmail.com>
2025-02-18 22:39:49 -0300

    fix: revert contract logic and simplified verification, since it is a single id

--- contracts/POAPVerifier.sol
+++ contracts/POAPVerifier.sol
@@ -4,59 +4,51 @@ pragma solidity 0.8.28;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IPOAP {
-    function balanceOf(address account, uint256 id) external view returns (uint256);
+    function balanceOf(
+        address account,
+        uint256 id
+    ) external view returns (uint256);
 }
 
 contract POAPVerifier is Ownable {
     address public poapContract;
-    uint256[] public requiredPoapIds;
+    uint256 public poapId;
 
     mapping(address => bool) public verifiedUsers;
     address[] public verifiedUserList;
 
     event UserVerified(address indexed user);
-    event PoapIdsUpdated(uint256[] newPoapIds);
 
-    constructor(uint256[] memory _initialPoapIds, address _poapContract) Ownable(msg.sender) {
+    constructor(uint256 _poapId, address _poapContract) Ownable(msg.sender) {
         poapContract = _poapContract;
-        requiredPoapIds = _initialPoapIds;
+        poapId = _poapId;
     }
 
     /**
-     * @dev Updates the list of required POAP IDs. Can only be called by the owner.
-     * @param _newPoapIds The new array of required POAP IDs.
-     */
-    function updatePoapIds(uint256[] memory _newPoapIds) public onlyOwner {
-        requiredPoapIds = _newPoapIds;
-        emit PoapIdsUpdated(_newPoapIds);
-    }
-
-    /**
-     * @dev Checks if a user holds all required POAPs and registers them if eligible.
+     * @dev Checks if a user holds the required POAP and registers them.
      * @param _user The address of the user to verify.
      */
     function checkAndRegister(address _user) public {
         require(!verifiedUsers[_user], "User already verified");
-        require(checkEligibility(_user), "User does not have all required POAPs");
+        require(
+            checkEligibility(_user),
+            "User does not have the required POAP"
+        );
 
-        verifiedUsers[_user] = true;
         verifiedUserList.push(_user);
+        verifiedUsers[_user] = true;
+
         emit UserVerified(_user);
     }
 
     /**
-     * @dev Checks if a user meets the POAP requirements without registering them.
+     * @dev Checks if a user has the POAP required without registering them.
      * @param _user The address of the user to check.
      * @return true if the user has all required POAPs, false otherwise.
      */
     function checkEligibility(address _user) public view returns (bool) {
         IPOAP poap = IPOAP(poapContract);
-        for (uint256 i = 0; i < requiredPoapIds.length; i++) {
-            if (poap.balanceOf(_user, requiredPoapIds[i]) == 0) {
-                return false;
-            }
-        }
-        return true;
+        return poap.balanceOf(_user, poapId) > 0;
     }
 
     /**
--- test/POAPVerifierTest.t.sol
+++ test/POAPVerifierTest.t.sol
@@ -2,132 +2,69 @@
 pragma solidity 0.8.28;
 
 import "forge-std/Test.sol";
-import "../contracts/MockPOAP.sol";
 import "../contracts/POAPVerifier.sol";
 
+contract POAPMock {
+    mapping(address => mapping(uint256 => uint256)) public balances;
+
+    function setBalance(address user, uint256 id, uint256 amount) public {
+        balances[user][id] = amount;
+    }
+
+    function balanceOf(
+        address account,
+        uint256 id
+    ) external view returns (uint256) {
+        return balances[account][id];
+    }
+}
+
 contract POAPVerifierTest is Test {
-    MockPOAP mockPoap;
+    POAPMock poapMock;
     POAPVerifier poapVerifier;
-    address owner = address(this);
-    address addr1 = address(0x1);
-    address addr2 = address(0x2);
-    address addr3 = address(0x3);
-    address addr4 = address(0x4);
+    address user1 = address(0x1);
+    address user2 = address(0x2);
+    uint256 testPoapId = 42;
+    event UserVerified(address indexed user);
 
     function setUp() public {
-        // Deploy a mock POAP contract
-        mockPoap = new MockPOAP();
-
-        // Declare a dynamic uint256 array in memory
-        uint256[] memory poapIds = new uint256[](4);
-        poapIds[0] = 1;
-        poapIds[1] = 2;
-        poapIds[2] = 3;
-        poapIds[3] = 4;
-
-        // Deploy the POAPVerifier contract with the correctly formatted array
-        poapVerifier = new POAPVerifier(poapIds, address(mockPoap));
+        poapMock = new POAPMock();
+        poapVerifier = new POAPVerifier(testPoapId, address(poapMock));
     }
 
-    function testMintPoaps() public {
-        // Mint POAPs to different addresses and verify balances
-
-        // Address 1 receives 1 type of POAP
-        mockPoap.mint(addr1, 1);
-        assertEq(mockPoap.balanceOf(addr1, 1), 1);
-
-        // Address 2 receives 2 types of POAPs
-        mockPoap.mint(addr2, 1);
-        mockPoap.mint(addr2, 2);
-        assertEq(mockPoap.balanceOf(addr2, 1), 1);
-        assertEq(mockPoap.balanceOf(addr2, 2), 1);
-
-        // Address 3 receives 2 types of POAPs, 2 of each
-        uint256[] memory ids = new uint256[](2);
-        ids[0] = 2;
-        ids[1] = 3;
-        uint256[] memory amounts = new uint256[](2);
-        amounts[0] = 2;
-        amounts[1] = 2;
-        mockPoap.mintBatch(addr3, ids, amounts, "");
-
-        assertEq(mockPoap.balanceOf(addr3, 2), 2);
-        assertEq(mockPoap.balanceOf(addr3, 3), 2);
-
-        // Address 4 receives 4 different POAPs
-        uint256[] memory allIds = new uint256[](4);
-        allIds[0] = 1;
-        allIds[1] = 2;
-        allIds[2] = 3;
-        allIds[3] = 4;
-        uint256[] memory allAmounts = new uint256[](4);
-        allAmounts[0] = 1;
-        allAmounts[1] = 1;
-        allAmounts[2] = 1;
-        allAmounts[3] = 1;
-        mockPoap.mintBatch(addr4, allIds, allAmounts, "");
-
-        assertEq(mockPoap.balanceOf(addr4, 1), 1);
-        assertEq(mockPoap.balanceOf(addr4, 2), 1);
-        assertEq(mockPoap.balanceOf(addr4, 3), 1);
-        assertEq(mockPoap.balanceOf(addr4, 4), 1);
+    function testUserCanRegisterWithPoap() public {
+        poapMock.setBalance(user1, testPoapId, 1);
+        poapVerifier.checkAndRegister(user1);
+        assertTrue(poapVerifier.verifiedUsers(user1));
     }
 
-    function testVerifyUsers() public {
-        // Mint all required POAPs to addr4 to make them eligible
-        uint256[] memory ids = new uint256[](4);
-        ids[0] = 1;
-        ids[1] = 2;
-        ids[2] = 3;
-        ids[3] = 4;
-        uint256[] memory amounts = new uint256[](4);
-        amounts[0] = 1;
-        amounts[1] = 1;
-        amounts[2] = 1;
-        amounts[3] = 1;
-        mockPoap.mintBatch(addr4, ids, amounts, "");
-
-        // The user should pass the verification
-        poapVerifier.checkAndRegister(addr4);
-        assertTrue(poapVerifier.verifiedUsers(addr4));
-
-        // Address 1 does not have all required POAPs, should revert
-        vm.expectRevert("User does not have all required POAPs");
-        poapVerifier.checkAndRegister(addr1);
+    function testUserCannotRegisterWithoutPoap() public {
+        vm.expectRevert("User does not have the required POAP");
+        poapVerifier.checkAndRegister(user2);
     }
 
-    function testCannotRegisterTwice() public {
-        // Mint all required POAPs to addr4
-        uint256[] memory ids = new uint256[](4);
-        ids[0] = 1;
-        ids[1] = 2;
-        ids[2] = 3;
-        ids[3] = 4;
-        uint256[] memory amounts = new uint256[](4);
-        amounts[0] = 1;
-        amounts[1] = 1;
-        amounts[2] = 1;
-        amounts[3] = 1;
-        mockPoap.mintBatch(addr4, ids, amounts, "");
-
-        // First registration should succeed
-        poapVerifier.checkAndRegister(addr4);
-
-        // Second registration should revert
+    function testUserCannotRegisterTwice() public {
+        poapMock.setBalance(user1, testPoapId, 1);
+        poapVerifier.checkAndRegister(user1);
         vm.expectRevert("User already verified");
-        poapVerifier.checkAndRegister(addr4);
+        poapVerifier.checkAndRegister(user1);
     }
 
-    function testOnlyOwnerCanUpdatePoaps() public {
-        // Only the owner should be able to update the required POAP IDs
-        uint256[] memory newPoaps = new uint256[](2);
-        newPoaps[0] = 1;
-        newPoaps[1] = 3;
-        poapVerifier.updatePoapIds(newPoaps);
+    function testUserVerifiedEventEmitted() public {
+        poapMock.setBalance(user1, testPoapId, 1);
+        vm.expectEmit(true, true, false, false);
+        emit UserVerified(user1);
+        poapVerifier.checkAndRegister(user1);
+    }
 
-        // A non-owner should not be able to update POAP IDs
-        vm.prank(addr1);
-        vm.expectRevert();
-        poapVerifier.updatePoapIds(newPoaps);
+    function testGetVerifiedUsers() public {
+        poapMock.setBalance(user1, testPoapId, 1);
+        poapMock.setBalance(user2, testPoapId, 1);
+        poapVerifier.checkAndRegister(user1);
+        poapVerifier.checkAndRegister(user2);
+        address[] memory users = poapVerifier.getVerifiedUsers();
+        assertEq(users.length, 2);
+        assertEq(users[0], user1);
+        assertEq(users[1], user2);
     }
 }
